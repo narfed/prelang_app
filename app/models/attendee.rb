686cf8d603e11f1bf8ad0ea2321d51b9313dd7d5
Prelang Builder <builder@prelang.com>
2014-08-14 00:02:23 +0000

    Defined validation for attribute 'emmergencycontactphone' on model 'Attendee'

--- app/models/attendee.rb
+++ app/models/attendee.rb
@@ -3,4 +3,5 @@ class Attendee < ActiveRecord::Base
   belongs_to :program_segment
   belongs_to :user
   validates_formatting_of :phone, using: :us_phone
+  validates_formatting_of :emmergencycontactphone, using: :us_phone
 end
